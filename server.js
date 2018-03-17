01ab7eb344b28796967ede1e6c5f71e5ea42d28d
Steven Inouye <steveninouye@msn.com>
2018-03-17 07:07:57 -1000

    added routes needed

--- server.js
+++ server.js
@@ -7,6 +7,30 @@ app.get('/', (req, res) => {
   res.send('hello');
 });
 
+app.get('/buzzwords', (req, res) => {
+  res.send('hello');
+});
+
+app.post('/buzzwords', (req, res) => {
+  res.send('hello');
+});
+
+app.put('/buzzwords', (req, res) => {
+  res.send('hello');
+});
+
+app.delete('/buzzwords', (req, res) => {
+  res.send('hello');
+});
+
+app.post('/reset', (req, res) => {
+  res.send('hello');
+});
+
+app.post('/heard', (req, res) => {
+  res.send('hello');
+});
+
 app.listen(PORT, () => {
   console.log(`Server Listening on port ${PORT}`);
 });
